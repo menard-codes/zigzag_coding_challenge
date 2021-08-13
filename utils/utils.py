9ff55c405b09a07d62ee9dbb5545d747bc91eee2
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2021-08-13 23:37:13 +0800

    fixed docstring of util - reduce_to_letters

--- utils/utils.py
+++ utils/utils.py
@@ -3,15 +3,14 @@ from re import findall
 
 def reduce_to_letters(string: str) -> str:
     """
-    Removes special characters, numbers, punctuations, etc.
-    from the string input.
+    Removes special characters, numbers, punctuations, etc. from
+    the string input.
 
         Parameter:
-            string (str): The string to be reduced to letters
-            only and covert into lowercase.
+            string (str): The string to be reduced to letters only.
 
         Returns:
-            reduced_string (str): The reduced version of the string
+            reduced_string (str): The reduced version of the string.
     """
 
     letters_regex = r'[A-Za-z]+'
